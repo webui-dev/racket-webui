8d9c1c774d6aa9cbaaf33c57bc121774456366a7
Showns <comsystemsuser2@gmail.com>
2025-08-15 19:03:27 -0400

    Adding Info

---
+++ info.rkt
@@ -0,0 +1,9 @@
+#lang info
+
+(define name 'racket-webui)
+(define collection 'racket-webui)
+(define version "0.0.1")
+(define deps '("base" "ffi-lib" "ffi-definer" "ffi-atomic" "hash"))
+(define pkg-desc "Use any web browser or WebView as GUI, with Racket in the backend and modern web technologies in the frontend.")
+(define pkg-authors '("WebUI.me"))
+(define homepage "https://github.com/webui-dev/racket-webui")
